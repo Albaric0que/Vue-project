487505dfaa9ab329cf7d1d5dd8e0c184436a3c44
Albaric0que <albamrus@gmail.com>
2023-04-05 18:50:58 +0200

    Uploading changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h1>Esta es la página about</h1>
+    </div>
+    <div>
+      <h3>
+        Lorem fistrum ese que llega no te digo trigo por no llamarte Rodrigor llevame al sircoo se
+        calle ustée. Amatomaa jarl va usté muy cargadoo qué dise usteer a gramenawer no te digo
+        trigo por no llamarte Rodrigor. Va usté muy cargadoo al ataquerl diodeno no puedor a wan no
+        te digo trigo por no llamarte Rodrigor te voy a borrar el cerito la caidita caballo blanco
+        caballo negroorl de la pradera a peich. Pecador torpedo sexuarl te voy a borrar el cerito.
+      </h3>
+    </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    height: 100vh;
   }
 }
 </style>
---
+++ src/views/MyView.vue
@@ -0,0 +1,27 @@
+<script>
+import SingleComponent from '../components/mycomponents/RawHtmlData.vue'
+export default {
+  components: { SingleComponent }
+}
+</script>
+<template>
+  <div>
+    <h1>My Single Page</h1>
+    <h3>
+      Lorem fistrum ese que llega no te digo trigo por no llamarte Rodrigor llevame al sircoo se
+      calle ustée. Amatomaa jarl va usté muy cargadoo qué dise usteer a gramenawer no te digo trigo
+      por no llamarte Rodrigor. Va usté muy cargadoo al ataquerl diodeno no puedor a wan no te digo
+      trigo por no llamarte Rodrigor te voy a borrar el cerito la caidita caballo blanco caballo
+      negroorl de la pradera a peich. Pecador torpedo sexuarl te voy a borrar el cerito.
+    </h3>
+    --------------------------------------------------------------------------
+    <SingleComponent></SingleComponent>
+    <SingleComponent></SingleComponent>
+    <SingleComponent></SingleComponent>
+  </div>
+</template>
+<style lang="css" scoped>
+h1 {
+  color: red;
+}
+</style>
